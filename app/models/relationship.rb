e8de15f927a8688e930edb24170decc0b5f4667d
jl2005 <jiaolongdy@163.com>
2013-12-19 22:43:26 +0800

    save

---
+++ app/models/relationship.rb
@@ -0,0 +1,6 @@
+class Relationship < ActiveRecord::Base
+	belongs_to :follower, class_name: "User"
+	validates :follower_id, presence: true
+	validates :followed_id, presence: true
+	belongs_to :followed, class_name: "User"
+end
